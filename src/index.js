3a55304891bf806259795e7da1ddbddb25cfb08f
Nikita Lilhore <102968844+Lnikita123@users.noreply.github.com>
2022-09-11 16:14:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express=require('express')
+const express=require('express')  //express has various functions in which we will be using the router function for establishing the connection.
 const app=express()
 
 const route=require('./routes/route')
 
 const bodyparser=require('body-parser')
-const multer=require('multer')
+const multer=require('multer') // img format or any other file format.
  app.use(bodyparser.json())
  app.use(bodyparser.urlencoded({extended:true}))
  app.use(multer().any())
@@ -17,6 +17,6 @@ mongoose.connect('mongodb+srv://tara:rVnCoeQUcaLqFprm@cluster0.k1yor.mongodb.net
 
 app.use('/',route)
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {  //setting up the environment variable and the server will be listening to the port 3000.
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
